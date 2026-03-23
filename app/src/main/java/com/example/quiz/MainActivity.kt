23f29984cefe68b12b4813c7aa46772da850c9a0
DAVID LUCAS DOS SANTOS <25132827@dsn127m1246729.cfp127aluno.local>
2026-03-23 16:57:06 -0300

    Iniciada

---
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.quiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quiz.screens.InicioScreen
+import com.example.quiz.ui.theme.QuizTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuizTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    InicioScreen (
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+            }
+        }
+    }
+}
